471b421eeb22729b0f45cc94d5c33d73f62a5e7d
USER <brojekma@gmail.com>
2023-05-14 12:02:53 +0200

    New user addition to db example

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(
     "mysql+pymysql://root:Brojekag99@localhost:3306/company"
 )
+Session = sessionmaker(bind=engine)
 Base = declarative_base(bind=engine)
 
 
